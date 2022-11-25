32ccb598a4ecc2af784117aadfd82253bd2b0955
marek.lipan@datasentics.com <marek.lipan@datasentics.com>
2022-11-25 19:20:34 +0000

    Merge conflict commit

--- challenge6_data.py
+++ challenge6_data.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-from pyspark.sql.functions import col
+import pyspark.sql.functions as F
 
 # COMMAND ----------
 
@@ -8,7 +8,56 @@ display(labs_data_df)
 
 # COMMAND ----------
 
-display(labs_data_df.select("Patient").distinct())
+"30.12.1899 11:38:00"
+
+# COMMAND ----------
+
+transformed_labs_data_df = (
+    labs_data_df
+    .withColumn("Value", F.coalesce(F.col("ValueNumber"), F.col("ValueText")))
+    .withColumn("Time", F.to_timestamp("EntryTime"))
+    .select(["Patient", "EntryDate", "EntryTime", "Time", "NCLP", "Value"])
+)
+
+# COMMAND ----------
+
+display(transformed_labs_data_df)
+
+# COMMAND ----------
+
+display(labs_data_df.count())
+
+# COMMAND ----------
+
+display(labs_data_df.select("ID").distinct().count())
+
+# COMMAND ----------
+
+display(labs_data_df.select("Report").distinct().count())
+
+# COMMAND ----------
+
+display(labs_data_df.select("Patient"))
+
+# COMMAND ----------
+
+display(labs_data_df.select("Code", "NCLP", "Analyte").group)
+
+# COMMAND ----------
+
+labs_data_df.filter(col("NCLP").isNotNull()).count()
+
+# COMMAND ----------
+
+69343 / 979175
+
+# COMMAND ----------
+
+labs_data_df.filter(col("Code").isNotNull()).count()
+
+# COMMAND ----------
+
+display(labs_data_df.select("NCLP").distinct().count())
 
 # COMMAND ----------
 
